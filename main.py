503f45c280a4e221ea0ae2099f3303f74a4564e6
Brian Donohue <brian.donohue89@gmail.com>
2015-09-03 13:47:22 -0400

    Initial commit
    Signed-off-by: Brian Donohue <brian.donohue89@gmail.com>

---
+++ main.py
@@ -0,0 +1,28 @@
+import re
+import os
+from pymongo import MongoClient
+from flask import Flask, make_response, render_template, redirect, abort, request, jsonify
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+client = MongoClient(os.environ['MONGOLAB_URI'])
+db = client.get_default_database()
+db.posts.ensure_index([
+    ('name', 1),
+    ('caption', 1)
+])
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def search():
+    q = request.form['q']
+    search_query = {"$or": [
+        {"name": {"$regex": q}},
+        {"caption": {"$regex": q}}
+    ]}
+    posts = db.posts.find(search_query)
+    return render_template('index.html', posts=posts)
+
